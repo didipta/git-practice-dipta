31a08fab317966ed440145ef5f0fab2e2f846b55
Dipta saha <66532706+didipta@users.noreply.github.com>
2026-04-28 21:45:39 +0600

    Add basic calculator functions

--- src/main.py
+++ src/main.py
@@ -1,4 +1,8 @@
 from datetime import date
+from utils import add, subtract
 
 print("Name: Dipta Saha")
 print("Today's Date:", date.today())
+
+print("Add Result:", add(10, 5))
+print("Subtract Result:", subtract(10, 5))
