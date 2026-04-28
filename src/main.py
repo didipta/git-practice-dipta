c3e96b7d63564611bee750fb01fa4c1b46daac2e
Dipta saha <66532706+didipta@users.noreply.github.com>
2026-04-28 22:06:07 +0600

    Improve main.py code formatting

--- src/main.py
+++ src/main.py
@@ -3,11 +3,8 @@ from utils import add, subtract, multiply, divide
 
 print("Name: Dipta Saha")
 print("Today's Date:", date.today())
-
 print("Add Result:", add(10, 5))
 print("Subtract Result:", subtract(10, 5))
-
 print("Multiply Result:", multiply(10, 5))
-
 print("Divide Result:", divide(10, 0))
 print("Divide Result:", divide(10, 2))
